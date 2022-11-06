64de56c1fdfd6d7d5863f79a7c774f387b660abd
Rafael Camargo <hello@rafaelcamargo.com>
2022-11-06 14:48:19 -0300

    feat: add view footer

--- src/base/components/view/view.js
+++ src/base/components/view/view.js
@@ -1,5 +1,6 @@
 import './view.styl';
 import { Header } from '@src/base/components/header/header';
+import { Link } from '@src/base/components/link/link';
 
 export const View = ({ children, heading }) => {
   return (
@@ -8,6 +9,12 @@ export const View = ({ children, heading }) => {
       <article className="fdw-view-content">
         {children}
       </article>
+      <footer className="fdw-view-footer">
+        Created and curated by
+        <Link href="https://rafaelcamargo.com/" external>
+          Rafael Camargo
+        </Link>
+      </footer>
     </main>
   );
 };
