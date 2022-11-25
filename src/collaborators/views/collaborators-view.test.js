a6d877b9fe09efaef1357b3412fbdb9b0857bd8e
Rafael Camargo <hello@rafaelcamargo.com>
2022-11-25 12:28:45 -0300

    feat: create collaborators view

---
+++ src/collaborators/views/collaborators-view.test.js
@@ -0,0 +1,27 @@
+import { render, within, RouterMock } from '@src/base/services/testing';
+import { CollaboratorsView } from './collaborators-view';
+
+describe('Collaborators View', () => {
+  function mount(){
+    return render(
+      <RouterMock>
+        <CollaboratorsView />
+      </RouterMock>
+    );
+  }
+
+  it('should contain logo and heading', () => {
+    const { getByAltText, getByRole } = mount();
+    expect(getByAltText('Four Day Week\'s Logo')).toBeInTheDocument();
+    expect(getByRole('heading', { name: 'Collaborators' })).toBeInTheDocument();
+  });
+
+  it('should contain a collaborators list', () => {
+    const { getByText } = mount();
+    const secondCollaboratorItem = getByText(/lucas cunha/i).parentElement;
+    expect(getByText(/andré pereira/i)).toBeInTheDocument();
+    expect(
+      within(secondCollaboratorItem).getByText(/learn more →/i)
+    ).toHaveAttribute('href', 'https://thegamingpub.com/');
+  });
+});
