7ded90a31f696d02c1dea2434a6db35349645825
Rafael Camargo <hello@rafaelcamargo.com>
2022-12-20 23:34:52 -0300

    feat: improve contribute page copy

--- src/contribute/views/contribute-view.js
+++ src/contribute/views/contribute-view.js
@@ -4,7 +4,7 @@ import { Link } from '@src/base/components/link/link';
 import { View } from '@src/base/components/view/view';
 
 export const ContributeView = () => {
-  const description = 'If you work in a 4-day week company or would like to suggest one to be added to the list, you can contribute to this project filling a Google Form or opening a pull request on Github.';
+  const description = 'If you work in a 4-day week company or would like to suggest one to be added to the list, you can contribute to this project by filling out a Google Form or opening a pull request on Github.';
   return (
     <div className="fdw-contribute-view">
       <View
